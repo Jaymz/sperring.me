33bf1df4635e74b5de8c778f5246346fea27deaa
James Sperring <james@sperring.me>
2016-03-23 22:39:41 +0000

    Initial admin screens added

---
+++ Blog/src/Blog/Controllers/PostsController.cs
@@ -0,0 +1,121 @@
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Data.Entity;
+using Blog.Data.Context;
+using Blog.Data.Entities;
+
+namespace Blog.Controllers
+{
+    public class PostsController : Controller
+    {
+        private BlogContext _context;
+
+        public PostsController(BlogContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Posts
+        public IActionResult Index()
+        {
+            return View(_context.Posts.ToList());
+        }
+
+        // GET: Posts/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Post post = _context.Posts.Single(m => m.Id == id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: Posts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Posts/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Posts.Add(post);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Post post = _context.Posts.Single(m => m.Id == id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Posts/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(post);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Delete/5
+        [ActionName("Delete")]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Post post = _context.Posts.Single(m => m.Id == id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Post post = _context.Posts.Single(m => m.Id == id);
+            _context.Posts.Remove(post);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
